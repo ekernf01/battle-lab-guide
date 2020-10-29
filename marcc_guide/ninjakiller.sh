e6ea15409b53b3135d8d75e661481e9c584c5dd6
Ashton Omdahl <aomdahl@gmail.com>
2020-10-29 13:56:16 -0400

    Update ninjakiller.sh
    New version more comprehensive capture of functions and smoother cleanup

--- marcc_guide/ninjakiller.sh
+++ marcc_guide/ninjakiller.sh
@@ -4,38 +4,43 @@
 ninjakiller(){
     str_match=$1
     kill_list=$(ps -eaf | grep ${USER}| cut -f3 -d " ")
+    user_id=$(echo "${USER}" | cut -f1 -d "@")
+    altkill_list=$(ps aux | grep -i "${user_id}*" | grep -v "grep" | grep -v "^root" \
+                | awk '{print $2}')
+    safekill_list=$(ps aux | grep -i "${user_id}*" | grep -v "grep" | grep -v "^root" \
+                | grep -v "${user_id}@jhu.edu@pts" | grep -v "\-bash\b" | awk '{print $2}')
     if [[ ${str_match} == "-h" ]]; then
         echo 1>&2 -e "
         Syntax:\n
-        ninjakiller scan : Lists job associated with user\n
-        ninjakiller go : Terminates user associated jobs without warn\n
-        ninjakiller : Lists currently running job IDs, and prompts to terminate them all (response y/n)\n\n
+        ninjakiller scan : Scans job associated to user\n
+        ninjakiller go : Kills user associated jobs without warn\n
+        ninjakiller : Warns job kill\n\n
         Your User ID appears to be: ${USER}\n"
         return
     elif [[ ${str_match} == "scan" ]];then
         echo -e "Your User ID appears to be: ${USER}\n"
-        for each in ${kill_list};do
-            echo -e "JOB detail for PID (${each})\n$(ps ax|egrep "^${each}")"
+        for each in ${altkill_list};do
+            echo -e "\nJOB detail for PID (${each})\n$(ps -up ${each})"
         done
-        echo -e "For selective kill(do): kill -9 <PID>(listedabove)"
+        echo -e "\nFor selective kill(do): kill -9 <PID>(listedabove)"
         return
     elif [[ ${str_match} == "go" ]];then
         echo -e "Preparing ninja kill run:\n"
-        for each_pid in ${kill_list}; do
+        for each_pid in ${altkill_list}; do
             echo -e "Killing PID: ${each_pid}"
-            kill -9 ${each_pid};
+            kill -9 ${each_pid} &> /dev/null;
         done
         echo -e "command success!\n";
         return
     elif [[ ${str_match} == "" ]];then
         echo -e "Your User ID appears to be: ${USER}\n"
-        echo -e "Ninja job listing: ${kill_list}\n"
+        echo -e "Ninja job listing:\n${safekill_list}\n"
         while true; do
         read -p "Do you wish to kill these jobs? " yn
         case $yn in
-            [Yy]* ) for each_pid in ${kill_list}; do
+            [Yy]* ) for each_pid in ${safekill_list}; do
                         echo -e "Killing PID: ${each_pid}"
-                        kill -9 ${each_pid};
+                        kill -9 ${each_pid} &> /dev/null;
                     done;
                     echo -e "command success!\n"; break;;
             [Nn]* ) echo -e "\nlooking for?: ninjakiller scan\n";break;;
